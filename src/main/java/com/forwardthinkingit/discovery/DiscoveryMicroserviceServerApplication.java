4b0aa9961504650aead60174eb0ae3ac70dd4f8d
Krystan Honour <krystan@forward-thinking-it.com>
2017-08-11 13:04:20 +0100

    updated

--- src/main/java/com/forwardthinkingit/discovery/DiscoveryMicroserviceServerApplication.java
+++ src/main/java/com/forwardthinkingit/discovery/DiscoveryMicroserviceServerApplication.java
@@ -1,4 +1,4 @@
-package com.forwardthinkingit;
+package com.forwardthinkingit.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
